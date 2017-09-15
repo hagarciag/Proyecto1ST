a9281e4a691ee68153b18e26e6fbb57cbe381a82
Hernan Alonso Garcia Gallego <hagarciag@gmail.com>
2017-09-15 17:00:28 -0500

    Nuevos  akjkustyes

--- R/fitDistribution.R
+++ R/fitDistribution.R
@@ -4,47 +4,63 @@ binner <- function(var) {
   require(shiny)
   library(MASS)
   library(fitdistrplus)
+  library(shinythemes)
   set.seed(101)
   shinyApp(
 
     # Define UI for random distribution app ----
     ui <- fluidPage(
 
+      theme = shinytheme("darkly"),
+
       # App title ----
-      titlePanel("Tabsets"),
+      titlePanel("Temporal Series"),
 
       # Sidebar layout with input and output definitions ----
       sidebarLayout(
 
         # Sidebar panel for inputs ----
         sidebarPanel(
+          fileInput('file1', 'Import data',
+                    accept=c('text/csv',
+                             'text/comma-separated-values,text/plain',
+                             '.csv')),
+
+          checkboxInput('header', 'Header', TRUE),
 
           # Input: Select the random distribution type ----
-          radioButtons("dist", "Distribution type:",
-                       c("Normal" = "norm",
-                         "Uniform" = "unif",
-                         "Log-normal" = "lnorm",
-                         "Exponential" = "exp")),
+          # checkboxGroupInput
+          checkboxGroupInput("dist", "Distribution type:",
+                             c("Normal" = "norm",
+                               "Uniform" = "unif",
+                               "Log-normal" = "lnorm",
+                               "Exponential" = "exp",
+                               "Chi-Squared" = "chisq",
+                               "Logistic" = "logis",
+                               "Cauchy" = "cauchy"), selected = "norm"),
 
           # br() element to introduce extra vertical spacing ----
-          br(),
+          br()
 
-          # Input: Slider for the number of observations to generate ----
-          sliderInput("n",
-                      "Number of observations:",
-                      value = 500,
-                      min = 1,
-                      max = 1000)
+          # # Input: Slider for the number of observations to generate ----
+          # sliderInput("n",
+          #  "Number of observations:",
+          #  value = 500,
+          #  min = 1,
+          #   max = 1000)
 
         ),
+
 
         # Main panel for displaying outputs ----
         mainPanel(
 
           # Output: Tabset w/ plot, summary, and table ----
           tabsetPanel(type = "tabs",
-                      tabPanel("Graficas", plotOutput("plot")),
-                      tabPanel("Estadisticos", verbatimTextOutput("summary"))
+                      tabPanel("Plot", plotOutput("plot")),
+                      tabPanel("Summary", verbatimTextOutput("summary")),
+                      tabPanel("Table", tableOutput("table"))
+
           )
 
         )
@@ -63,9 +79,24 @@ binner <- function(var) {
                        unif = runif,
                        lnorm = rlnorm,
                        exp = rexp,
-                       rnorm)
+                       chisq = rchisq,
+                       logis = rlogis,
+                       cauchy = rcauchy)
 
         dist(input$n)
+
+        datos <- input$file1
+
+        if (is.null(datos))
+          datos <- var
+
+
+
+
+        ##file1 <- input$file
+        ##if(is.null(file1)){return()}
+        ##read.table(file=file1$datapath, sep=input$sep, header = input$header, stringsAsFactors = input$stringAsFactors)
+
       })
 
       # Generate a plot of the data ----
@@ -74,64 +105,131 @@ binner <- function(var) {
       # both tracked, and all expressions are called in the sequence
       # implied by the dependency graph.
       output$plot <- renderPlot({
+        datos <- input$file1
+        if (is.null(datos))
+          muestra <- var
+        else{
+          if(input$header)
+          {
+            muestra = scan(datos$datapath,skip=1)
+          }
+          else{
+            muestra = scan(datos$datapath)
+          }
+        }
+
         dist <- input$dist
-        n <- input$n
+        # n <- input$n
 
 
-        hist(var, prob=TRUE,
-             main = paste("r", dist, "(", n, ")", sep = ""),
+        hist(muestra, prob=TRUE,main = paste("Hernancho"),
              col = "#75AADB", border = "white", ylab = "Probabilidad")
 
-
-
-
-        den <- density(var)
+        den <- density(muestra)
         lines(den, col = 'red', lwd = 2)
 
-        AjustmentTypes<-c("Aproximación empirica","Distribución teorica")
-        if(dist=='norm'){
-          fit <- fitdistr(var, densfun="normal")
-          #hist(d(), pch=20, breaks=25, prob=TRUE, main="")
-          curve(dnorm(x, fit$estimate[1], fit$estimate[2]), col="green", lwd=2, add=T)
-          #print("Normal")
+        AjustmentTypes<-c("Aproximación empirica")
+        VectorColores <- c("red")
 
-          AjustmentTypes<-c("Aproximación empirica","Distribución teorica Normal")
+
+        for (i in 1:length(input$dist)){
+          if(dist[i]=='norm'){
+            fit <- fitdistr(muestra, densfun="normal")
+            #hist(d(), pch=20, breaks=25, prob=TRUE, main="")
+            curve(dnorm(x, fit$estimate[1], fit$estimate[2]), col="blue", lwd=2, add=T)
+            #print("Normal")
+
+            AjustmentTypes<-c(AjustmentTypes,"Distribución teorica Normal")
+            VectorColores <- c(VectorColores,"blue")
+          }
+
+          if(dist[i]=='lnorm'){
+            #fit <- fitdistr(d(), "lnorm")
+            fit <- fitdist(muestra, "lnorm")
+            curve(dlnorm(x, fit$estimate[1], fit$estimate[2]), col="orange", lwd=2, add=T)
+            AjustmentTypes<-c(AjustmentTypes,"Distribución teorica LNormal")
+            VectorColores <- c(VectorColores,"orange")
+          }
+
+          if(dist[i]=='unif'){
+            x <- fitdist(muestra, "unif", method="mle")
+            a <- x$estimate[1]
+            b <- x$estimate[2]
+            curve(dunif(x,min = a,max = b), col="green", lwd=2, add=T)
+            AjustmentTypes<-c(AjustmentTypes,"Distribución teorica Uniforme")
+            VectorColores <- c(VectorColores,"green")
+          }
+
+          if(dist[i]=='chisq'){
+            chi_df <- fitdistr(muestra,"chi-squared",start=list(df=2),method="BFGS") ## Fitting
+            chi_k <- chi_df[[1]][1] ## Degrees of freedom
+            curve(dchisq(x,df=chi_k), col="purple", lwd=2, add=T)
+            AjustmentTypes<-c(AjustmentTypes,"Distribución teorica Chi-cuadrado")
+            VectorColores <- c(VectorColores,"purple")
+          }
+
+          if(dist[i]=='logis'){
+            fit <- fitdistr(muestra, densfun="logistic")
+            curve(dlogis(x, fit$estimate[1], fit$estimate[2]), col="gray", lwd=2, add=T)
+            AjustmentTypes<-c(AjustmentTypes,"Distribución teorica Logistica")
+            VectorColores <- c(VectorColores,"gray")
+          }
+
+          if(dist[i]=='cauchy'){
+            fit <- fitdistr(muestra, densfun="cauchy")
+            curve(dcauchy(x, fit$estimate[1], fit$estimate[2]), col="pink", lwd=2, add=T)
+            AjustmentTypes<-c(AjustmentTypes,"Distribución teorica Cauchy")
+            VectorColores <- c(VectorColores,"pink")
+          }
+
+          if(dist[i]=='exp'){
+            fit <- fitdistr(muestra, "exponential")
+            curve(dexp(x, rate = fit$estimate), col = "gold", lwd=2, add = TRUE)
+            AjustmentTypes<-c(AjustmentTypes,"Distribución teorica Exponencial")
+            VectorColores <- c(VectorColores,"gold")
+          }
         }
 
-        if(dist=='lnorm'){
-          #fit <- fitdistr(d(), "lnorm")
-          fit <- fitdist(var, "lnorm")
-          curve(dlnorm(x, fit$estimate[1], fit$estimate[2]), col="green", lwd=2, add=T)
-          #AjustmentTypes<-c("Aproximación empirica","Distribución teorica LNormal")
-          AjustmentTypes<-c("Aproximación empirica","Distribución teorica LNorm")
-        }
 
-        if(dist=='exp'){
-          #fit <- fitdistr(d(), "lnorm")
-          fit <- fitdistr(var, "exponential")
-          #curve(dlnorm(x, fit$estimate[1], fit$estimate[2]), col="green", lwd=2, add=T)
-          curve(dexp(x, rate = fit$estimate), col = "green", lwd=2, add = TRUE)
-          #AjustmentTypes<-c("Aproximación empirica","Distribución teorica LNormal")
-          AjustmentTypes<-c("Aproximación empirica","Distribución teorica Exp")
-          print(ks.test(var, "pexp", fit$estimate)) # p-value > 0.05 -> distribution not refused
-        }
 
         #legend("topright", col=c('red', 'green'), legend=nombres, bty="o")
-        legend("topright", legend=AjustmentTypes, fill=c('red', 'green'), col=c('red', 'green'),
+        legend("topright", legend=AjustmentTypes, fill=VectorColores, col=VectorColores,
                bty="n")
-
 
       })
 
       # Generate a summary of the data ----
-      output$summary <- renderTable({
+      output$summary <- renderPrint({
+        datos <- input$file1
+        if (is.null(datos))
+          return(NULL)
+        if(input$header)
+        {
+          muestra = scan(datos$datapath,skip=1)
+        }
+        else{
+          muestra = scan(datos$datapath)
+        }
+        summary(muestra)
+      })
 
-        data.frame(
+      # Generate an HTML table view of the data ----
+      ## output$table <- renderTable({
+      ##   d()
+      ## })
 
-          as.character(summary(var)))
-
-
-
+      output$table <- renderTable({
+        datos <- input$file1
+        if (is.null(datos))
+          return(NULL)
+        if(input$header)
+        {
+          muestra = scan(datos$datapath,skip=1)
+        }
+        else{
+          muestra = scan(datos$datapath)
+        }
+        muestra
       })
 
     }
@@ -143,17 +241,17 @@ binner <- function(var) {
 }
 
 
-n<-1000
-x<-rnorm(n,0,1)
-y<-rlnorm(n,100,5)
-z<-rexp(n,1.85)
-w<-runif(n)
-hist(x,freq=TRUE)
+#n<-1000
+#x<-rnorm(n,0,1)
+#y<-rlnorm(n,100,5)
+#z<-rexp(n,1.85)
+#w<-runif(n)
+#hist(x,freq=TRUE)
 #Normal
-binner(x)
+#binner(x)
 #Lognormal
-binner(y)
+#binner(y)
 #Exponencial
-binner(z)
+#binner(z)
 #Uniforme
-binner(w)
+#binner(w)
