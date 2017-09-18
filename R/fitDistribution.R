7e0f997002da483c11bcdd8a4c1a744b15324dbf
Hernan Alonso Garcia Gallego <hagarciag@gmail.com>
2017-09-17 19:48:20 -0500

    Se incluye tabla de estadisticos y se quita el tema porque afectabala tabla

--- R/fitDistribution.R
+++ R/fitDistribution.R
@@ -27,13 +27,14 @@ binner <- function(var) {
   library(MASS)
   library(fitdistrplus)
   library(shinythemes)
+  library(DT)
   set.seed(101)
   shinyApp(
 
     # Define UI for random distribution app ----
     ui <- fluidPage(
 
-      theme = shinytheme("darkly"),
+      #theme = shinytheme("darkly"),
 
       # App title ----
       titlePanel("Temporal Series"),
@@ -79,7 +80,7 @@ binner <- function(var) {
 
           # Output: Tabset w/ plot, summary, and table ----
           tabsetPanel(type = "tabs",
-                      tabPanel("Plot", plotOutput("plot"),br(),verbatimTextOutput("ensayo")),
+                      tabPanel("Plot", plotOutput("plot"),br(),DT::dataTableOutput('estadisticos'),br(),verbatimTextOutput("ensayo")),
                       tabPanel("Summary", verbatimTextOutput("summary")),
                       tabPanel("Table", tableOutput("table"))
 
@@ -145,72 +146,113 @@ binner <- function(var) {
         dist <- input$dist
         # n <- input$n
 
-
-        hist(muestra, prob=TRUE,main = paste("Hernancho"),
+        hist(muestra, prob=TRUE,main = paste("Ajuste de funciones"),
              col = "#75AADB", border = "white", ylab = "Probabilidad")
 
         den <- density(muestra)
+        media_muestral<-mean(muestra)
         lines(den, col = 'red', lwd = 2)
 
         AjustmentTypes<-c("Aproximación empirica")
         VectorColores <- c("red")
-
+        Media <- c(media_muestral)
+        Distribucion <- c("Empirica")
+        List_Fits <- list()
+        AIC<- c(NA)
 
         for (i in 1:length(input$dist)){
           if(dist[i]=='norm'){
-            fit <- fitdistr(muestra, densfun="normal")
-            #hist(d(), pch=20, breaks=25, prob=TRUE, main="")
-            curve(dnorm(x, fit$estimate[1], fit$estimate[2]), col="blue", lwd=2, add=T)
-            #print("Normal")
+            fit_normal <- fitdistr(muestra, densfun="normal")
+            curve(dnorm(x, fit_normal$estimate[1], fit_normal$estimate[2]), col="blue", lwd=2, add=T)
 
             AjustmentTypes<-c(AjustmentTypes,"Distribución teorica Normal")
             VectorColores <- c(VectorColores,"blue")
+            Media <- c(Media,fit_normal$estimate[1])
+            Distribucion <- c(Distribucion, "Normal")
+            List_Fits[["fit_normal"]] <- fit_normal
+            AIC <- c(AIC,AIC(fit_normal))
+            #print(fit_normal$estimate)
+            #print(fit_normal$sd)
+            #print(fit_normal$vcov)
+            #print(AIC(fit_normal))
           }
 
           if(dist[i]=='lnorm'){
-            #fit <- fitdistr(d(), "lnorm")
-            fit <- fitdist(muestra, "lnorm")
-            curve(dlnorm(x, fit$estimate[1], fit$estimate[2]), col="orange", lwd=2, add=T)
+            #fit_lnormal <- fitdistr(d(), "lnorm")
+            fit_lnormal <- fitdist(muestra, "lnorm")
+            curve(dlnorm(x, fit_lnormal$estimate[1], fit_lnormal$estimate[2]), col="orange", lwd=2, add=T)
             AjustmentTypes<-c(AjustmentTypes,"Distribución teorica LNormal")
             VectorColores <- c(VectorColores,"orange")
+            Media <- c(Media,fit_lnormal$estimate[1])
+            Distribucion <- c(Distribucion, "LogNormal")
+            List_Fits[["fit_lnormal"]] <- fit_lnormal
+            AIC <- c(AIC,fit_lnormal$aic)
+            #print(fit_lnormal$aic)
+            #print(fit_lnormal$bic)
           }
 
           if(dist[i]=='unif'){
-            x <- fitdist(muestra, "unif", method="mle")
-            a <- x$estimate[1]
-            b <- x$estimate[2]
+            fit_unif <- fitdist(muestra, "unif", method="mle")
+            a <- fit_unif$estimate[1]
+            b <- fit_unif$estimate[2]
             curve(dunif(x,min = a,max = b), col="green", lwd=2, add=T)
             AjustmentTypes<-c(AjustmentTypes,"Distribución teorica Uniforme")
             VectorColores <- c(VectorColores,"green")
+            Media <- c(Media,0.5*(a+b))
+            Distribucion <- c(Distribucion, "Uniforme")
+            List_Fits[["fit_unif"]] <- fit_unif
+            AIC <- c(AIC,fit_unif$aic)
+            print(fit_unif$aic)
           }
 
           if(dist[i]=='chisq'){
-            chi_df <- fitdistr(muestra,"chi-squared",start=list(df=2),method="BFGS") ## Fitting
-            chi_k <- chi_df[[1]][1] ## Degrees of freedom
+            fit_chisq <- fitdistr(muestra,"chi-squared",start=list(df=2),method="BFGS") ## Fitting
+            chi_k <- fit_chisq[[1]][1] ## Degrees of freedom
             curve(dchisq(x,df=chi_k), col="purple", lwd=2, add=T)
             AjustmentTypes<-c(AjustmentTypes,"Distribución teorica Chi-cuadrado")
             VectorColores <- c(VectorColores,"purple")
+            Media <- c(Media,chi_k)
+            Distribucion <- c(Distribucion, "Chi-Cuadrado")
+            List_Fits[["fit_chisq"]] <- fit_chisq
+            AIC <- c(AIC,AIC(fit_chisq))
           }
 
           if(dist[i]=='logis'){
-            fit <- fitdistr(muestra, densfun="logistic")
-            curve(dlogis(x, fit$estimate[1], fit$estimate[2]), col="gray", lwd=2, add=T)
+            fit_logist <- fitdistr(muestra, densfun="logistic")
+            curve(dlogis(x, fit_logist$estimate[1], fit_logist$estimate[2]), col="gray", lwd=2, add=T)
             AjustmentTypes<-c(AjustmentTypes,"Distribución teorica Logistica")
             VectorColores <- c(VectorColores,"gray")
+            Media <- c(Media,fit_logist$estimate[1])
+            Distribucion <- c(Distribucion, "Logistica")
+            List_Fits[["fit_logist"]] <- fit_logist
+            AIC <- c(AIC,AIC(fit_logist))
           }
 
           if(dist[i]=='cauchy'){
-            fit <- fitdistr(muestra, densfun="cauchy")
-            curve(dcauchy(x, fit$estimate[1], fit$estimate[2]), col="pink", lwd=2, add=T)
+            fit_cauchy <- fitdistr(muestra, densfun="cauchy")
+            curve(dcauchy(x, fit_cauchy$estimate[1], fit_cauchy$estimate[2]), col="pink", lwd=2, add=T)
             AjustmentTypes<-c(AjustmentTypes,"Distribución teorica Cauchy")
             VectorColores <- c(VectorColores,"pink")
+            Media <- c(Media,NA)
+            Distribucion <- c(Distribucion, "Cauchy")
+            List_Fits[["fit_cauchy"]] <- fit_cauchy
+            AIC <- c(AIC,AIC(fit_cauchy))
           }
 
           if(dist[i]=='exp'){
-            fit <- fitdistr(muestra, "exponential")
-            curve(dexp(x, rate = fit$estimate), col = "gold", lwd=2, add = TRUE)
+            fit_exp <- fitdistr(muestra, "exponential")
+            curve(dexp(x, rate = fit_exp$estimate), col = "gold", lwd=2, add = TRUE)
+            #ks<-ks.test(muestra, "pexp", fit_exp$estimate)
+            #print(ks$p.value)
+            #print(ks$statistic)
+            #print(ks$p.value)
+
             AjustmentTypes<-c(AjustmentTypes,"Distribución teorica Exponencial")
             VectorColores <- c(VectorColores,"gold")
+            Media <- c(Media,1/fit_exp$estimate)
+            Distribucion <- c(Distribucion, "Exponencial")
+            List_Fits[["fit_exp"]] <- fit_exp
+            AIC <- c(AIC,AIC(fit_exp))
           }
         }
 
@@ -220,19 +262,32 @@ binner <- function(var) {
         legend("topright", legend=AjustmentTypes, fill=VectorColores, col=VectorColores,
                bty="n")
 
+        #gofstat(List_Fits, fitnames = Distribucion)
+
+        estadisticos <- data.frame(Media, AIC)
+        row.names(estadisticos) <- Distribucion
+        output$estadisticos <- DT::renderDataTable(
+          DT::datatable(estadisticos, options = list(searching = FALSE, paging = FALSE, bLengthChange = FALSE))
+        )
+
+
+
+
       })
 
       # Generate a summary of the data ----
       output$summary <- renderPrint({
         datos <- input$file1
         if (is.null(datos))
-          return(NULL)
-        if(input$header)
-        {
-          muestra = scan(datos$datapath,skip=1)
-        }
-        else{
-          muestra = scan(datos$datapath)
+          muestra <- var
+		else{
+			if(input$header)
+			{
+			muestra = scan(datos$datapath,skip=1)
+			}
+			else{
+			muestra = scan(datos$datapath)
+			}
         }
         summary(muestra)
       })
@@ -245,23 +300,8 @@ binner <- function(var) {
       output$table <- renderTable({
         datos <- input$file1
         if (is.null(datos))
-          return(NULL)
-        if(input$header)
-        {
-          muestra = scan(datos$datapath,skip=1)
-        }
-        else{
-          muestra = scan(datos$datapath)
-        }
-        muestra
-      })
-
-      #Generacion Tabla
-      output$ensayo <- renderPrint({
-        datos <- input$file1
-        if (is.null(datos))
           muestra <- var
-        else{
+		else{
 			if(input$header)
 			{
 			muestra = scan(datos$datapath,skip=1)
@@ -270,9 +310,29 @@ binner <- function(var) {
 			muestra = scan(datos$datapath)
 			}
         }
-        summary(muestra)
-
+        muestra
       })
+
+
+        #Generacion Tabla
+        output$ensayo <- renderPrint({
+          datos <- input$file1
+          if (is.null(datos))
+            muestra <- var
+          else{
+            if(input$header)
+            {
+              muestra = scan(datos$datapath,skip=1)
+            }
+            else{
+              muestra = scan(datos$datapath)
+            }
+          }
+          #summary(muestra)
+          summary(muestra)
+
+        })
+
 
     }
 
@@ -284,8 +344,8 @@ binner <- function(var) {
 
 
 #n<-1000
-#x<-rnorm(n,50,1)
-#y<-rlnorm(n,100,5)
+#x<-rnorm(n,84,20)
+#y<-rlnorm(n,4,1)
 #z<-rexp(n,1.85)
 #w<-runif(n)
 #hist(x,freq=TRUE)
